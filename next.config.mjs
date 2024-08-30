3d1dcd0d5014d67d86fe3d43433edc6b751d8773
Mubeen-Ul-Hassan <mubeenulhassan396@gmail.com>
2024-08-30 11:03:51 +0500

    CORs fix

--- next.config.mjs
+++ next.config.mjs
@@ -10,34 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        // Routes this applies to
-        source: "/brief/(.*)",
-        // Headers
-        headers: [
-          // Allow for specific domains to have access or * for all
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "api.sanity.io",
-            // DOES NOT WORK
-            // value: process.env.ALLOWED_ORIGIN,
-          },
-          // Allows for specific methods accepted
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET",
-          },
-          // Allows for specific headers accepted (These are a few standard ones)
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
